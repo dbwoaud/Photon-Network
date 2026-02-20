f54394bc1d35aa5d4160fc3bd0310a24c0319bdc
dbwoaud <ryujm3410@naver.com>
2026-02-20 19:44:11 +0900

    (2026. 02. 20) 룸 시스템

--- Assets/Scripts/Lobby Manager.cs
+++ Assets/Scripts/Lobby Manager.cs
@@ -1,13 +1,49 @@
 using UnityEngine;
 using Photon.Pun;
+using System.Collections.Generic;
+using Photon.Realtime;
 
 public class LobbyManager : MonoBehaviourPunCallbacks
 {
+    [SerializeField] Transform parentTransform;
+    [SerializeField] Dictionary<string,GameObject> roomDictionary = new Dictionary<string,GameObject>();
+
+    public void GenerateRoom()
+    {
+        PanelManager.Instance.Load(Panel.GENERATOR);
+    }
+    public override void OnJoinedRoom()
+    {
+        PhotonNetwork.LoadLevel("Game");
+    }
     public override void OnConnectedToMaster()
     {
         if(PhotonNetwork.InLobby == false)
             PhotonNetwork.JoinLobby();
     }
-    
-    public override void OnRoomListUpdate
+
+    public override void OnRoomListUpdate(List<RoomInfo> roomList)
+    {
+        GameObject roomObject = null;
+        foreach (RoomInfo room in roomList)
+        {
+            if (room.RemovedFromList)
+            {
+                if (roomDictionary.TryGetValue(room.Name, out roomObject))
+                {
+                    roomDictionary.Remove(room.Name);
+                    Destroy(roomObject);
+                }             
+            }
+            else
+            {
+                if(!roomDictionary.TryGetValue(room.Name, out roomObject))
+                {
+                    roomObject = Instantiate(Resources.Load<GameObject>("Room"), parentTransform);
+                    roomDictionary.Add(room.Name, roomObject);
+                }
+                roomObject.GetComponent<RoomData>().UpdateRoomInfo(room);
+            }
+        }
+    }
 }
--- Assets/Scripts/Room Data.cs
+++ Assets/Scripts/Room Data.cs
@@ -2,11 +2,27 @@ using UnityEngine;
 using Photon.Pun;
 using UnityEngine.UI;
 using Photon.Realtime;
+using System;
 public class RoomData : MonoBehaviourPunCallbacks
 {
     [SerializeField] Button button;
     [SerializeField] Text roomText;
     [SerializeField] string titleText;
+    [SerializeField] RoomInfo roomInfo;
+    [SerializeField] event Action OnEntered;
+
+
+    public override void OnEnable()
+    {
+        base.OnEnable();
+        OnEntered += UpdateRoomStatus;
+    }
+
+    public override void OnDisable()
+    {
+        base.OnDisable();
+        OnEntered -= UpdateRoomStatus;
+    }
     public void Start()
     {
         button.onClick.AddListener(() => PhotonNetwork.JoinRoom(titleText));
@@ -14,12 +30,19 @@ public class RoomData : MonoBehaviourPunCallbacks
 
     public void UpdateRoomInfo(RoomInfo roomInfo)
     {
+        this.roomInfo = roomInfo;
         titleText = roomInfo.Name;
-        roomText.text = roomInfo.Name + " ( " + roomInfo.PlayerCount + " / " + roomInfo.MaxPlayers + " )"; 
+        roomText.text = roomInfo.Name + " ( " + roomInfo.PlayerCount + " / " + roomInfo.MaxPlayers + " )";
+        OnEntered?.Invoke();
     }
 
     public void UpdateRoomStatus()
     {
+        if (roomInfo.IsOpen)
+            button.interactable = true;
+        else
+            button.interactable = false;
 
     }
+
 }
