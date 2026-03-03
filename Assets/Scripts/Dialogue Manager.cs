0bfca840e0a5b1f1155017e4418cb81989d2a1cc
dbwoaud <ryujm3410@naver.com>
2026-03-03 19:44:07 +0900

    (2026. 03. 03) 원격 함수 호출

---
+++ Assets/Scripts/Dialogue Manager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using Photon.Pun;
+using UnityEngine.UI;
+using Photon.Chat;
+public class DialogueManager : MonoBehaviourPunCallbacks
+{
+    [SerializeField] InputField inputField;
+    [SerializeField] ScrollRect scrollRect;
+    [SerializeField] Transform parentTransform;
+
+    [PunRPC]
+    public void Send(string message)
+    {
+        GameObject chat = Instantiate(Resources.Load<GameObject>("Talk"), parentTransform);
+        chat.GetComponent<Text>().text = message;
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            inputField.ActivateInputField();
+            if (inputField.text.Length <= 0)
+            {
+                return;
+            }
+            string talk = PhotonNetwork.LocalPlayer.NickName + ": " + inputField.text;
+            photonView.RPC("Send", RpcTarget.All, talk);
+            inputField.text = "";
+            inputField.ActivateInputField();
+        }
+    }
+
+
+    
+}
